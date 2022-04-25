23e14325f0a73b9f6c46368c4b806ddbec026dfa
Alin Babu <alinbabu2010@gmail.com>
2022-04-25 15:52:34 +0530

    Add top header section composable UI and associated string resource

--- app/src/main/java/com/jetpack/tip/MainActivity.kt
+++ app/src/main/java/com/jetpack/tip/MainActivity.kt
@@ -3,41 +3,78 @@ package com.jetpack.tip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpack.tip.ui.theme.JetTipAppTheme
+import com.jetpack.tip.ui.theme.Purple100
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            RootView()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RootView() {
+    ContentView {
+        TopHeader()
+    }
+}
+
+@Composable
+fun ContentView(content: @Composable () -> Unit) {
+    JetTipAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun TopHeader(totalPerPerson: Double = 120.00) {
+    val total = "%.2f".format(totalPerPerson)
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Purple100
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.total_per_person),
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetTipAppTheme {
-        Greeting("Android")
     }
 }
