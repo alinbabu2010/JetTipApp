cb78242aac9e048c76b62f633b16d5793e2c4312
Alin Babu <alinbabu2010@gmail.com>
2022-04-25 17:09:57 +0530

    Add input field to main content

--- app/src/main/java/com/jetpack/tip/MainActivity.kt
+++ app/src/main/java/com/jetpack/tip/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.jetpack.tip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jetpack.tip.components.InputField
 import com.jetpack.tip.ui.theme.JetTipAppTheme
 import com.jetpack.tip.ui.theme.Purple100
 
@@ -82,9 +89,25 @@ fun TopHeader(totalPerPerson: Double = 120.00) {
 }
 
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
-fun MainContent(){
+fun MainContent() {
+    BillForm {
+        Log.d("TAG", "MainContent: $it")
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun BillForm(onValChange: (String) -> Unit) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -93,6 +116,17 @@ fun MainContent(){
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value)
+                    keyboardController?.hide()
+                }
+            )
 
         }
     }
