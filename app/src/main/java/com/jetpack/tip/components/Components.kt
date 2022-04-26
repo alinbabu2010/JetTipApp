2c155022a3ff6f77aad930704693e2cac609f9b8
Alin Babu <alinbabu2010@gmail.com>
2022-04-26 10:22:00 +0530

    Modify inputFiled modifiers and add new components

--- app/src/main/java/com/jetpack/tip/components/Components.kt
+++ app/src/main/java/com/jetpack/tip/components/Components.kt
@@ -1,20 +1,24 @@
 package com.jetpack.tip.components
 
+import android.util.Log
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.rounded.AttachMoney
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jetpack.tip.widgets.RoundIconButton
 
 @Composable
 fun InputField(
@@ -33,7 +37,9 @@ fun InputField(
         onValueChange = {
             valueState.value = it
         },
-        modifier = modifier,
+        modifier = modifier
+            .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
         label = { Text(text = labelId) },
         leadingIcon = {
             Icon(
@@ -48,4 +54,58 @@ fun InputField(
         textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground)
     )
 
+}
+
+
+@Composable
+fun SplitRow() {
+    var splitCount by remember {
+        mutableStateOf(1)
+    }
+    Row(
+        modifier = Modifier.padding(3.dp),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(text = "Split", modifier = Modifier.align(CenterVertically))
+        Spacer(modifier = Modifier.width(120.dp))
+        Row(
+            modifier = Modifier.padding(horizontal = 3.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            RoundIconButton(imageVector = Icons.Default.Remove, onClick = { splitCount -= 1 })
+            Text(
+                text = splitCount.toString(),
+                modifier = Modifier
+                    .align(CenterVertically)
+                    .padding(start = 9.dp, end = 9.dp)
+            )
+            RoundIconButton(imageVector = Icons.Default.Add, onClick = { splitCount += 1 })
+        }
+    }
+}
+
+@Composable
+fun TipRow() {
+    Row(modifier = Modifier.padding(horizontal = 3.dp, vertical = 12.dp)) {
+        Text(text = "Tip", modifier = Modifier.align(CenterVertically))
+        Spacer(modifier = Modifier.width(200.dp))
+        Text(text = "$33.00", modifier = Modifier.align(CenterVertically))
+    }
+}
+
+@Composable
+fun TipPercentageSlider(){
+    var sliderPositionState by remember {
+        mutableStateOf(0f)
+    }
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "$sliderPositionState%")
+        Spacer(modifier = Modifier.height(14.dp))
+        
+        //Slider
+        Slider(value = sliderPositionState, onValueChange = { newValue ->
+            sliderPositionState = newValue
+            Log.d("TAG", "TipPercentageSlider: $newValue")
+        })
+    }
 }
